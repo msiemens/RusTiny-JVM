1e6323566ed9dc9b38d25f1843da1cdbc0ebc3b9
Markus Siemens <markus@m-siemens.de>
2020-11-25 16:57:01 +0100

    feat: implement `!` operation

--- src/main/kotlin/de/msiemens/educomp/back/PrefixExpressionGenerator.kt
+++ src/main/kotlin/de/msiemens/educomp/back/PrefixExpressionGenerator.kt
@@ -2,6 +2,7 @@ package de.msiemens.educomp.back
 
 import de.msiemens.educomp.front.ast.Expression
 import de.msiemens.educomp.front.ast.Node
+import de.msiemens.educomp.front.ast.Type
 import de.msiemens.educomp.front.ast.UnaryOp
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
@@ -13,7 +14,21 @@ class PrefixExpressionGenerator(
     fun generate(op: UnaryOp, expression: Node<Expression>) {
         // https://github.com/apache/groovy/blob/12dca398e711ce6c703998b46c928f6bc87f0018/src/main/java/org/codehaus/groovy/classgen/asm/UnaryExpressionHelper.java#L50-L84
         when (op) {
-            UnaryOp.Not -> TODO()
+            UnaryOp.Not -> when (val type = codegen.types[expression.id]) {
+                Type.Bool -> {
+                    codegen.genExpression(expression)
+                    writer.visitInsn(Opcodes.ICONST_1)
+                    writer.visitInsn(Opcodes.IXOR)
+                }
+                Type.Int -> {
+                    codegen.genExpression(expression)
+                    writer.visitInsn(Opcodes.ICONST_M1)
+                    writer.visitInsn(Opcodes.IXOR)
+                }
+                else -> {
+                    error("Called UnaryOp.Not with unsupported type $type")
+                }
+            }
             UnaryOp.Neg -> {
                 codegen.genExpression(expression)
                 writer.visitInsn(Opcodes.INEG)
---
+++ src/test/resources/tests/run-pass/codegen/int-not.rs
@@ -0,0 +1,9 @@
+fn main() {
+    if !false {
+        println(!0);
+    }
+
+    if !true {
+        println(0);
+    }
+}
